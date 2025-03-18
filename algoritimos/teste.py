a6d7b2272625970afbf7a2b9c83be8ade002931f
MetalHeadW <mateusdwendhausen@gmail.com>
2025-03-18 19:12:50 -0300

    just playing

---
+++ algoritimos/teste.py
@@ -0,0 +1,14 @@
+##nmAluno: str
+##idade: int
+##salario: float
+##trabalhaAtualmente: bool
+
+#como funciona o sistema do python
+nmAluno = 'Mateus'
+idade = 21
+salario = 2500
+trabalhaAtualmente = True
+pcAumento = 15
+
+print(nmAluno)
+print(salario + salario * pcAumento/100)
---
+++ algoritimos/voexplodiopc.py
@@ -0,0 +1,15 @@
+#nmAluno: str
+#idade: int
+#salario: float
+#trabalhaAtualmente: bool
+
+
+print('hello world') 
+
+num1 =  float(input('insira o primeiro numero: '))
+num2 = float(input('insira o segundo numero: '))
+num3 =  float(input('insira o terceiro numero: '))
+resultado = ( num1 + num2 + num3)/3
+print('mestre dos magos falou que o aluno tirou', resultado)
+
+
