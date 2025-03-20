03899c11e58982856a205eff27fe629de97748cf
MetalHeadW <mateusdwendhausen@gmail.com>
2025-03-19 21:02:31 -0300

    updating my stuff

--- algoritimos/voexplodiopc.py
+++ algoritimos/voexplodiopc.py
@@ -11,5 +11,6 @@ num2 = float(input('insira o segundo numero: '))
 num3 =  float(input('insira o terceiro numero: '))
 resultado = ( num1 + num2 + num3)/3
 print('mestre dos magos falou que o aluno tirou', resultado)
-
+print('ei você é hora de morfar')
+input('aperte qualquer tecla pra explodir seu com puta dor =)')
 
