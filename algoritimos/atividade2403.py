342de77c5d7d2c84840f9c4d543e2a7e09d71381
MetalHeadW <mateusdwendhausen@gmail.com>
2025-03-24 21:18:40 -0300

    atividades

---
+++ algoritimos/atividade2403.py
@@ -0,0 +1,32 @@
+#a: int
+#b: int
+#c: int
+#x: float
+#y: float
+#z: float
+#nome: str
+#rua: str
+#l1: bool
+#l2: bool
+
+#a = 1
+#b = 2
+#c = 3
+#x = 2.0
+#y = 10.0
+#z = -1.0
+#nome = "João"
+#rua = "rua 1"
+#l1 = True
+#l2 = False 
+
+#nome:str = "João"
+#idade:int = 25
+#altura:float = 1.75
+#pi = 3.14159265359
+
+#print(f"Nome: {nome}, Idade: {idade}, Altura: {altura}m")
+#print("Nome:{}, Idade: {}, Altura: {}m".format(nome, idade, altura))
+
+#print(f"o valor de pi com duas casas decimais é {pi:.2f}")
+#print("O valor de pi com duas casas decimais é: {:.2f}".format(pi))
