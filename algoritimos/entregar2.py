5c9aee765ea2026629bc9e4a91c110c804875b4e
MetalHeadW <mateusdwendhausen@gmail.com>
2025-04-03 20:59:51 -0300

    socrates??? onde?

--- algoritimos/entregar2.py
+++ algoritimos/entregar2.py
@@ -1,2 +1,10 @@
-'''escreva um programa, que entra duas datas, e determine qual delas ocorreu cronológicamente primeiro, cada data é composta por 3 numeros
-inteiros, cada um representando o dia, outro o mes, e outro o ano'''
+'''faça um programa que ordene três numeros em ordem crescente'''
+print("")
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+print("")
+
+lista=[num1, num2, num3]
+
+print(sorted(lista))
