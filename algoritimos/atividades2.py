8b2b9d1d2641964161f80f55bc640cbaab4242ad
MetalHeadW <mateusdwendhausen@gmail.com>
2025-04-03 19:23:08 -0300

    ai, aiaiai, aiaiaiaiaiaiai em cima embaixo e puxa e vai

---
+++ algoritimos/atividades2.py
@@ -0,0 +1,7 @@
+'''faça um programa que ordene três numeros em ordem crescente'''
+print("")
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+print("")
+
