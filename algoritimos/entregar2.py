16e88d01fef725390217600c0d3bc8b448c147d1
MetalHeadW <mateusdwendhausen@gmail.com>
2025-04-03 19:34:39 -0300

    odeio minha vida amorosa

---
+++ algoritimos/entregar2.py
@@ -0,0 +1,2 @@
+'''escreva um programa, que entra duas datas, e determine qual delas ocorreu cronológicamente primeiro, cada data é composta por 3 numeros
+inteiros, cada um representando o dia, outro o mes, e outro o ano'''
