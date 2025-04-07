bf2ce5cc36a0d897318c428c162a4954d892c636
MetalHeadW <mateusdwendhausen@gmail.com>
2025-04-07 20:49:03 -0300

    bendita prova

--- algoritimos/entregar5.py
+++ algoritimos/entregar5.py
@@ -3,4 +3,4 @@ letra = input("Digite uma letra:")
 if letra == 'a' or letra == 'e'or letra == 'i'or letra == 'o'or letra == 'u':
     print("a letra é uma vogal")
 else:
-    print("a letra é uma consoante")
+    print("a letra é uma consoante")    
---
+++ avaliação0704.py
@@ -0,0 +1,14 @@
+'''Cada produto no estoque contém as seguintes informações:
+Nome do produto
+Quantidade atual do produto em estoque.
+Quantidade mínima permitida.
+Quantidade máxima permitida.
+O usuário deve informar os dados do produto e qual operação que deseja fazer:
+1 - Adicionar no estoque
+2 - retirar do estoque
+Ao escolher a operação, deve perguntar quantos itens deseja adicionar ou retirar, conforme a operação
+escolhida.
+Verificar se a operação pode ser feita, considerando a quantidade mínima ou a quantidade máxima e
+tomar ações apropriadas (exibir mensagens de alerta no caso de operação não permitida e exibir o novo
+estoque no caso de a operação ser permitida)'''
+
