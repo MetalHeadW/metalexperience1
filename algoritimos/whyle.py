2eb9243085c9e5ddf1e8067d948313260d35f57c
MetalHeadW <mateusdwendhausen@gmail.com>
2025-04-14 21:31:36 -0300

    eu to me arrependendo de ter pedido pra voce voltar, puta que me pariu mulher azeda

---
+++ algoritimos/whyle.py
@@ -0,0 +1,10 @@
+op = 0
+while op != 3:
+    op = int(input("1 - Entrada \n 2 - Saida \n 3 Finalizar\n"))
+    if op == 1:
+        print("entrada de produtos\n\n")
+    elif op==2:
+        print("saida de produtos\n\n")
+    elif op == 3:
+        print("adeus\n\n")
+        
---
+++ algoritimos/whyle2.py
@@ -0,0 +1,6 @@
+orcamento = float(input("Qual o valor você tem para gastar? "))
+
+while orcamento > 0:
+    vl = float(input("qual o valor da compra? "))
+    orcamento -= vl
+    print('Resta R$', orcamento)
