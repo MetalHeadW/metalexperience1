c15308e154416fb4f2b1b0d8aff6b63178fa2c97
MetalHeadW <mateusdwendhausen@gmail.com>
2025-04-14 21:55:35 -0300

    meudeus eu to confundindo o marcos com um tal de erik qm é erik?

---
+++ algoritimos/atividade4.py
@@ -0,0 +1,5 @@
+'''faça um programa que pergunte ao usuário 
+quantos produtos ele deseja comprar. 
+Para cada produto pergunte o valor unitário
+e a quantidade desekada, ao final, Mostre o total da compra
+Versão 2: Perguntar se deseja adicionar mais itens (S) ou (N)'''
