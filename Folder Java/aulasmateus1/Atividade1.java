b52a2000e90ed1bd4dded0aee4e4894ce7e42bbc
MetalHeadW <mateusdwendhausen@gmail.com>
2025-06-06 21:40:04 -0300

    Consideravelmente aceitável!

--- Folder Java/aulasmateus1/Atividade1.java
+++ Folder Java/aulasmateus1/Atividade1.java
@@ -1,5 +1,40 @@
 package aulasmateus1;
 
-public class Atividade1 {
+import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
+
+public class Atividade1 {
+	public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    String nome = JOptionPane.showInputDialog("Nome");
+	double peso = Double.parseDouble(JOptionPane.showInputDialog("Peso"));
+	double altura = Double.parseDouble(JOptionPane.showInputDialog("altura"));
+	
+    
+    double juros = calcularJurosSimples(capital, taxa, tempo);
+    
+    double montante = capital + juros;
+    
+    exibirResultado(juros, montante);
+    
+    scanner.close();
+   
+    
+}
+	public static double lerValor(Scanner scanner, String mensagem) {
+		System.out.print(mensagem);
+		return scanner.nextDouble();
+	}
+	
+	public static double calcularJurosSimples(double capital, double taxa, double tempo) {
+		return capital * taxa * tempo;
+	}
+	
+	public static void exibirResultado(double juros, double montante) {
+		System.out.printf("Juros Acumulados: R$ %.2f%n", juros);
+		System.out.printf("Montante final: R$ %.2f%n", montante);
+	}
 }
